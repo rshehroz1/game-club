9075b7fd1ff42649100372a1e961c1e9986d28d8
Blackxok <nasridoni1@gmail.com>
2023-02-22 22:55:21 +0500

    deleted swiper npm

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -2,3 +2,25 @@
 $(function () {
     $('.selectpicker').selectpicker();
 });
+// swiper functionality
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
